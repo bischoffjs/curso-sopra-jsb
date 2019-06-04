baa4ba02242e9faa83960bfc3385c72e01c8e417
bischoffjs <bischoffjs@gmail.com>
2019-06-04 10:01:58 +0200

    Cambios en el Ejercicio de SemiBlackJack

--- js/ejercicios/Ejercicio SemiBlackJack/app.js
+++ js/ejercicios/Ejercicio SemiBlackJack/app.js
@@ -1,12 +1,22 @@
+var puntosUser = 0;
+var puntosMaquina = 0;
+const numeroTope = 20;
 iniciar();
+
+function iniciar() {
+    jugarUser()
+    jugarMaquina();
+    validarGanador(puntosMaquina, puntosUser);
+    reinicio();
+}
 
 function getRandomNum() {
     return Math.ceil((Math.random() * 10) + 1);
 }
 
-function iniciar() {
-    var puntosUser = 0;
-    var puntosMaquina = 0;
+function jugarUser(){
+    puntosUser = 0;
+    puntosMaquina = 0;
     var resp = true;
     while (resp == true) {
         var resp = confirm('Quiere un numero?');
@@ -14,7 +24,7 @@ function iniciar() {
             var numAzar = getRandomNum()
             var puntosUser = puntosUser + numAzar;
             console.log('Puntos Usuario.: ' + puntosUser);
-            if (puntosUser > 20) {
+            if (puntosUser > numeroTope) {
                 console.log('Te has pasado de 20');
                 resp == false;
                 break;
@@ -27,21 +37,43 @@ function iniciar() {
 
 
     }
+}
 
+function jugarMaquina()
+{
     while (puntosMaquina <= 17) {
         var numAzar = getRandomNum()
         var puntosMaquina = puntosMaquina + numAzar;
         console.log('Puntos Maquina.: ' + puntosMaquina);
-        if (puntosMaquina > 20) {
+        if (puntosMaquina > numeroTope) {
             console.log('La Maquina se ha pasado de 20');
             resp == false;
             break;
         }
     }
+}
 
-    validarGanador(puntosMaquina, puntosUser);
+function validarGanador(puntosMaquina, puntosUser) {
+    if ((puntosMaquina > puntosUser) && (puntosMaquina <= numeroTope)) {
+        console.log("Maquina ha ganado");
+    }
+    else if ((puntosUser > puntosMaquina) && (puntosUser <= numeroTope)) {
+        console.log("User ha ganado");
+    }
+    else if ((puntosUser > numeroTope) && (puntosMaquina <= numeroTope)) {
+        console.log("Maquina ha ganado");
+    }
+    else if ((puntosMaquina > numeroTope) && (puntosUser <= numeroTope)) {
+        console.log("User ha ganado");
+    }
+    else if ((puntosMaquina > numeroTope) && (puntosUser > numeroTope)) {
+        console.log("Ambos han perdido");
+    }
+    else {
+        (puntosMaquina == puntosUser)
+        console.log('Empate');
+    }
 
-    reinicio();
 }
 
 function reinicio() {
@@ -55,25 +87,3 @@ function reinicio() {
     }
 }
 
-function validarGanador(puntosMaquina, puntosUser) {
-    if ((puntosMaquina > puntosUser) && (puntosMaquina <= 20)) {
-        console.log("Maquina ha ganado");
-    }
-    else if ((puntosUser > puntosMaquina) && (puntosUser <= 20)) {
-        console.log("User ha ganado");
-    }
-    else if ((puntosUser > 20) && (puntosMaquina <= 20)) {
-        console.log("Maquina ha ganado");
-    }
-    else if ((puntosMaquina > 20) && (puntosUser <= 20)) {
-        console.log("User ha ganado");
-    }
-    else if ((puntosMaquina > 20) && (puntosUser > 20)) {
-        console.log("Ambos han perdido");
-    }
-    else {
-        (puntosMaquina == puntosUser)
-        console.log('Empate');
-    }
-
-}
