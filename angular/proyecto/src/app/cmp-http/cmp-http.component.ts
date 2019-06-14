433d745a6cacd8adcaa4160c8a50322bf88e8e50
bischoffjs <bischoffjs@gmail.com>
2019-06-14 13:01:11 +0200

    ADICIONAL

---
+++ angular/proyecto/src/app/cmp-http/cmp-http.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { PeliculasService } from './peliculas.service';
+
+@Component({
+  selector: 'app-cmp-http',
+  templateUrl: './cmp-http.component.html',
+  styleUrls: ['./cmp-http.component.css']
+})
+export class CmpHttpComponent implements OnInit {
+  generos: Array<any> = [];
+  constructor(private peliculasService: PeliculasService) { }
+
+  ngOnInit() {
+    this.peliculasService.getGeneros().subscribe((datos) => {
+      this.generos = datos;
+    });
+  }
+
+}
---
+++ angular/proyecto/src/app/cmp-http/genero.ts
@@ -0,0 +1,6 @@
+export class Genero {
+    constructor(public id: string, public nombre: string ) {
+        
+    }
+}
+
